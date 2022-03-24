20eb312c2df477fe5f30cb949f9076c0e2105863
JanneVed <janven.jv@gmail.com>
2022-03-24 14:25:56 +0100

    Fixed getting all databases

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import { useState } from "react";
 import Select from "react-select";
 import styles from "../styles/Home.module.css";
 
-const HomePage = ({results}) => {
+const HomePage = (props) => {
+
+  //console.log(props.data);
 
   // makes an array of eployees with id and name.
   const getEmployeeInfo = () => {
     const person = [];
-    results.forEach((projectworker) => {
+    props.data[1].forEach((projectworker) => {
       person.push({
         id: projectworker.id,
         employee: projectworker.properties.Name.title[0].plain_text,
@@ -26,12 +28,12 @@ const HomePage = ({results}) => {
     });
     return employees;
   }
-  
+  // Show total hours for selected employee
   const getEmloyeeHours = (selected) => {
     let totalHours;
     let employeeHours = [];
     const selectedId = selected
-    results.forEach(element => {
+    props.data[1].forEach(element => {
       employeeHours.push({
         id: element.id,
         hours: element.properties['Total hours'].rollup.number
@@ -62,70 +64,60 @@ export default HomePage;
 
 export async function getStaticProps()
 {
-  for (let database = 1; database < 2; database++)
-  {
-    if (database == 0)
+  let data = [];
+  const notionClient = new Client(
     {
-      const notionClient = new Client(
-        {
-          auth: process.env.NOTION_TOKEN,
-        }
-      );
-      const database_id = process.env.NOTION_DATABASE_PROJECTS_ID;
-      const response = await notionClient.databases.query(
-        {
-          database_id,
-        }
-      );
-      console.log(response); // show in terminal
-      return{
-        props:
-        {
-          results: response.results
-        }
-      };
-    };
-    if (database == 1)
-    {
-      const notionClient = new Client(
-        {
-          auth: process.env.NOTION_TOKEN,
-        }
-      );
-      const database_id = process.env.NOTION_DATABASE_PEOPLE_ID;
-      const response = await notionClient.databases.query(
-        {
-          database_id,
-        }
-      );
-      console.log(response); // show in terminal
-      return{
-        props:
-        {
-          results: response.results
-        }
-      };
-    };
-    if (database == 2)
-    {
-      const notionClient = new Client(
-        {
-          auth: process.env.NOTION_TOKEN,
-        }
-      );
-      const database_id = process.env.NOTION_DATABASE_TIMEREPORTS_ID;
-      const response = await notionClient.databases.query(
-        {
-          database_id,
-        }
-      );
-      console.log(response); // show in terminal
-      return{
-        props:
-        {
-          results: response.results
-        }
-      };
-    };
+      auth: process.env.NOTION_TOKEN,
+    }
+  );
+  
+  data.push(await getProjects(notionClient));
+
+  data.push(await getPeople(notionClient));
+
+  data.push(await getTimereports(notionClient));
+
+  return {
+    props: {
+      data,
+    },
   };
 };
+// Get Projects
+async function getProjects(client)
+{
+  let response = await client.databases.query(
+    {
+      database_id: process.env.NOTION_DATABASE_PROJECTS_ID,
+    }
+  );
+
+  console.log("projects:" + response.results);
+
+  return response.results;
+};
+// Get People
+async function getPeople(client)
+{
+  let response = await client.databases.query(
+    {
+      database_id: process.env.NOTION_DATABASE_PEOPLE_ID,
+    }
+  );
+
+  console.log("people:" + response.results);
+
+  return response.results;
+}
+// Get Timereports
+async function getTimereports(client)
+{
+  let response = await client.databases.query(
+    {
+      database_id: process.env.NOTION_DATABASE_TIMEREPORTS_ID,
+    }
+  );
+  console.log("timereports:" + response.results);
+
+  return response.results;
+}
