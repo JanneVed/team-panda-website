c815ede102fb47c75f189e01f9e5fe3c2dbbd1f8
Linus Holmgren <linus.holmgren@edu.edugrade.se>
2022-03-25 12:45:22 +0100

    Added support for getting project info

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,37 @@ const HomePage = (props) => {
 
   //console.log(props.data);
 
-  // Makes an array of eployees with id and name.
+  // Makes an array of employees with id and name.
   const getEmployeeInfo = () => {
-    const person = [];
-    props.data[1].forEach((projectworker) => {
-      person.push({
-        id: projectworker.id,
-        employee: projectworker.properties.Name.title[0].plain_text,
-        total_hours: projectworker.properties['Total hours'].rollup.number});
+    const persons = [];
+    props.data[1].forEach(person => {
+      persons.push({
+        id: person.id,
+        employee: person.properties.Name.title[0].plain_text,
+        total_hours: person.properties['Total hours'].rollup.number});
     });
-    return person
+    return persons
   };
+  // Makes an array of all projects from notion database
+  const getProjectsInfo = () => {
+    const projects = [];
+    props.data[0].forEach(project => {
+      projects.push({
+        id: project.id,
+        projectName: project.properties.Projectname.title[0].plain_text,
+        status: project.properties.Status
+      });
+    });
+    return projects
+  };
+
+  // const getTimereportsInfo = () => {
+  //   const reports = [];
+  //   props.data[2].forEach(report => {
+  //     reports.push({
+  //     })
+  //   })
+  // }
 
   // Send the names for the employee to the select.
   const dropdownNames = () => {
@@ -25,7 +45,7 @@ const HomePage = (props) => {
       employees.push(<option value={element.id} key={element.id}>{element.employee}</option>)
     });
     return employees;
-  }
+  };
 
   // Show total hours for selected employee
   const getEmloyeeHours = (selected) => {
@@ -36,37 +56,66 @@ const HomePage = (props) => {
       }
     });
     return totalHours;
-  }
+  };
 
   // Calls getEmployeeInfo, compares selected person in dropdown and gets employee name
-  const getEmployeeName = (selected) => {
+  const getEmployeeName = (Id) => {
     let employeeName;
-
     getEmployeeInfo().forEach(element => {
-      if (selected === element.id) {
+      if (Id === element.id) {
         employeeName = element.employee
       }
     })
     return employeeName;
-  }
-  
+  };
+
+  const dropdownProjects = () => {
+    const projects = [];
+    getProjectsInfo().forEach(element => {
+      projects.push(<option value={element.id} key={element.id}>{element.projectName}</option>)
+    });
+    return projects;
+  };
+
+  const getProjectName = (Id) => {
+    let projectsName;
+    getProjectsInfo().forEach(element => {
+      if (Id === element.id) {
+        projectsName = element.projectName
+      }
+    });
+    return projectsName;
+  };
+
   // Home page return
-  const [Selects, setSelects] = useState();
+  const [PeopleId, setPeopleId] = useState();
+  const [ProjectId, setProjectId] = useState();
+
   return <div className={styles.content}>
     <div className={styles.homePageHeader}>
       <h1>Home Page</h1>
     </div>
     <form>
       <label form="people">Employee: </label>
-      <select value={Selects} onChange={e => setSelects(e.target.value)} className="people">
+      <select value={PeopleId} onChange={e => setPeopleId(e.target.value)} className="people">
         <option disabled selected>Select Employee</option>
         {dropdownNames()}
       </select>
     </form>
     <div className={styles.homePageContent}>
-      <p>Employee: {getEmployeeName(Selects)}</p>
-      <p>Total hours: {getEmloyeeHours(Selects)}</p>
-      {console.log("Employee Selected: "+ getEmployeeName(Selects) + "\n\n" + "User Id: " + Selects)}
+      <p>Date</p>
+      <p>Selected Person</p>
+      <p>Hours</p>
+      <form>
+        <label form="projects">Projects: </label>
+        <select value={ProjectId} onChange={e => setProjectId(e.target.value)} className="projects">
+          <option disabled selected>Select Project</option>
+          {dropdownProjects()}
+      </select>
+      </form>
+      {/* <p>Employee: {getEmployeeName(PeopleId)}</p>
+      <p>Total hours: {getEmloyeeHours(PeopleId)}</p> */}
+      {console.log(`Employee Selected: ${getEmployeeName(PeopleId)}\nTotal hours: ${getEmloyeeHours(PeopleId)}\nUser Id: ${PeopleId}\n\nProject Selected: ${getProjectName(ProjectId)}\nProject Id: ${ProjectId}`)}
     </div>
   </div>;
 };
